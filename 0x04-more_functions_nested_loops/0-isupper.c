483c1800fedaa739938f522ac9b45d9eba11cfd8
AlhajiTarzain <dredeen21@gmail.com>
2023-07-20 11:28:23 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if letter is capital
+ *
+ *Return: 1 for capital letter 0 for any else
+ */
+int _isupper(int c)
+{
+	if ( c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+		return (0);
+}
+
